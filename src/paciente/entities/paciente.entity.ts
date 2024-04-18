250c213c2a2ff9b50d05555723a8dfd2d9c59de1
Mario-Chavez <marioachavez1@gmail.com>
2024-04-18 15:19:21 -0300

    was added typeorm end it  was done entity paciente

--- src/paciente/entities/paciente.entity.ts
+++ src/paciente/entities/paciente.entity.ts
@@ -1,1 +1,31 @@
-export class Paciente {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Paciente {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  id_paciente: string;
+
+  @Column()
+  fullName: string;
+
+  @Column()
+  birthdate: string;
+
+  @Column({ nullable: true })
+  address?: string;
+
+  @Column({ nullable: true })
+  phone?: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  obraSocial: string;
+}
