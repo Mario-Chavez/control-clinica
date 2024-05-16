bd3b854a99ed9d98df950e95869bbe4ff50378fa
Mario-Chavez <marioachavez1@gmail.com>
2024-05-16 09:18:52 -0300

    change of strategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,11 +1,22 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Paciente } from 'src/paciente/entities/paciente.entity';
+import { Repository } from 'typeorm';
+import { Medico } from 'src/medico/entities/medico.entity';
+import { JwtPayload } from '../interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+  constructor(
+    @InjectRepository(Paciente)
+    private readonly pacienteRepository: Repository<Paciente>,
+    @InjectRepository(Medico)
+    private readonly medicoRepository: Repository<Medico>,
+    configService: ConfigService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,7 +24,23 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
-    return { userId: payload.id };
+  async validate(payload: JwtPayload) {
+    let user: Paciente | Medico;
+
+    const findUser = await this.pacienteRepository.findOneBy({
+      id: payload.id,
+    });
+    user = findUser
+      ? findUser
+      : await this.medicoRepository.findOneBy({
+          id: payload.id,
+        });
+
+    if (!user) throw new UnauthorizedException('token invalido');
+
+    if (user.isActive === false)
+      throw new UnauthorizedException('User not active');
+
+    return user;
   }
 }
