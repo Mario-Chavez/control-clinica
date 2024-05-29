55c0396633ed1bfe1a824a3e870c0395a6eda12d
Mario-Chavez <marioachavez1@gmail.com>
2024-05-29 11:44:02 -0300

    se utilizo el auth decorator en turnos

--- src/administrador/administrador.controller.ts
+++ src/administrador/administrador.controller.ts
@@ -11,6 +11,8 @@ import {
 import { AdministradorService } from './administrador.service';
 import { CreateAdministradorDto } from './dto/create-administrador.dto';
 import { UpdateAdministradorDto } from './dto/update-administrador.dto';
+import { Auth } from 'src/auth/decorator/auth.decorator';
+import { Role } from 'src/auth/enums/rol.enum';
 
 @Controller('administrador')
 export class AdministradorController {
@@ -21,16 +23,19 @@ export class AdministradorController {
     return this.administradorService.create(createAdministradorDto);
   }
 
+  @Auth(Role.ADMIN)
   @Get()
   findAll() {
     return this.administradorService.findAll();
   }
 
+  @Auth(Role.ADMIN)
   @Get(':id')
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.administradorService.findOne(id);
   }
 
+  @Auth(Role.ADMIN)
   @Patch(':id')
   update(
     @Param('id', ParseUUIDPipe) id: string,
@@ -39,6 +44,7 @@ export class AdministradorController {
     return this.administradorService.update(id, updateAdministradorDto);
   }
 
+  @Auth(Role.ADMIN)
   @Delete(':id')
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.administradorService.remove(id);
